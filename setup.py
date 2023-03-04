28e0f53ceed5e849aaeb16e53fe19458a793e99d
stephan <smuts1989@gmail.com>
2023-03-04 23:06:21 +0100

    corrected name information

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     name='my_pip_package',
     version=__version__,
 
-    url='https://github.com/MichaelKim0407/tutorial-pip-package',
-    author='Michael Kim',
-    author_email='mkim0407@gmail.com',
+    url='https://github.com/linguist89/anvil_designs',
+    author='Stephan Smuts',
+    author_email='smuts1989@gmail.com',
 
     py_modules=['my_pip_package'],
 )
